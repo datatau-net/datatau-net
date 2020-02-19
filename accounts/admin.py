3a7d992843ac311bcacbbd9776156d7e208e6f4a
Pedro Muñoz <pedro.munoz.botas@gmail.com>
2020-02-19 07:23:28 +0100

    added admin view for filtering and modifying trolls

--- accounts/admin.py
+++ accounts/admin.py
@@ -8,4 +8,8 @@ class CustomUserAdmin(UserAdmin):
     model = CustomUser
 
 
+UserAdmin.list_display += ('is_troll',)
+UserAdmin.list_filter += ('is_troll',)
+UserAdmin.fieldsets += (('Extra info', {'fields': ('is_troll',)}),)
+
 admin.site.register(CustomUser, CustomUserAdmin)
